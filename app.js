1bf5c484d853e5e6e357c6b404bd223208ebd3d2
Lucas Stampini Yokoyama <89997454+lucasstampini@users.noreply.github.com>
2022-11-15 17:57:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const menu = document.querySelector('.header .nav-bar .nav-list .menu');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+menu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
 });
 
 document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#222222';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#222222';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
 });
 
 menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+    item.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
 });
